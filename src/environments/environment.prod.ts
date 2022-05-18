106156314ad1194697343d623dc24acd6441d2d8
paranauerj <joaopaulocostarj@gmail.com>
2022-05-18 23:55:21 +0100

    Adequacao do codigo para versao de producao

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCwE2jxHHuBxYPJSkYg5gm9HQSPDJy3COU",
+    authDomain: "iwinery-74cdc.firebaseapp.com",
+    projectId: "iwinery-74cdc",
+    storageBucket: "iwinery-74cdc.appspot.com",
+    messagingSenderId: "183181791719",
+    appId: "1:183181791719:web:6eed29cf6fe6dfbf31ad42"
+  },
+  blockchain: {
+    url: "http://25.75.178.136:8001",
+    apiKey: "507e1a93c3cb189293914bf4e06e1d94"
+  }
 };
